9c4846067bcc7aff1547e900f8957a8dda96612d
gabriel073 <gabrielmarzioli@hotmail.com>
2024-11-02 11:08:48 -0300

    modify back in raiway

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,8 @@ import './index.css'
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:3001'
-// axios.defaults.baseURL = 'https://backend-pikapak-production.up.railway.app'
-axios.defaults.baseURL = 'https://backend-pikapak.vercel.app'
+axios.defaults.baseURL = 'https://backend-pikapak-production.up.railway.app'
+// axios.defaults.baseURL = 'https://backend-pikapak.vercel.app'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
